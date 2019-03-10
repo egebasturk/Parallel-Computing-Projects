5ef35a5f98fb704269a798ba62c53d5d44f29a4e
egebasturk <alp.ege.basturk@gmail.com>
2019-03-10 10:11:13 +0300

    Part B: II. Works with single node.

--- Project1/cs426_project1_code/matmult-mpi-1d.c
+++ Project1/cs426_project1_code/matmult-mpi-1d.c
@@ -46,15 +46,15 @@ int* convertRowWiseMatrixToColumnWise(const int arr[], int N)
 }
 int* matrixMatrixMultSerial(int *mat1, int *mat2, int dim1, int dim2) // Dim 2 is the big one. Output will be a square matrix because of project specs.
 {
-    //printf("dim1:%d dim2:%d\n", dim1, dim2);
+    printf("dim1:%d dim2:%d\n", dim1, dim2);
     int *result = (int*) malloc(dim1 * dim1 * sizeof(int));
     int sum_tmp = 0;
     for (int row = 0; row < dim1; ++row) {
         for (int column = 0; column < dim1; ++column) {
             for (int i = 0; i < dim2; ++i) {
-                sum_tmp += mat1[row * dim2 + i] * mat2[column + i * dim2];
+                sum_tmp += mat1[row * dim2 + i] * mat2[column * dim2 + i];
             }
-            //printf("col:%d/%d row:%d/%d\n", column, dim1, row, dim1);
+            printf("col:%d/%d row:%d/%d \tsumtmp: %d\n", column, dim1, row, dim1, sum_tmp);
             result[column + row * dim1] = sum_tmp;
             sum_tmp = 0;
         }
@@ -202,10 +202,10 @@ int main (int argc, char *argv[])
         readMatrixFromFile(&array2, input_filename2, &N);
         array2_colwise = convertRowWiseMatrixToColumnWise(array2, N);
 
-        result_array = malloc(N * N * sizeof(int));
         printf("Array length is %d\n", N*N);
         printf ("Sending data . . .\n");
     }
+    result_array  = malloc(N * N * sizeof(int));
     MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);               // Bcast array dimension
     // Split based on rows. Shouldn't be a problem since expecting square matrices.
     MPI_Barrier(MPI_COMM_WORLD);
@@ -234,17 +234,17 @@ int main (int argc, char *argv[])
             i++;
         }
     }
-    printf("%d\n", tmp);
+    /*printf("%d\n", tmp);
     for (int k = 0; k < tmp; ++k) {
         printf("%d\t", count_tmp[k]);
-    }
+    }*/
 
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_Group_incl(world_group, tmp, count_tmp, &master_world_group);
     MPI_Comm master_comm;
     MPI_Comm_create_group(MPI_COMM_WORLD, master_world_group, MASTER_TAG, &master_comm);
-    if (master_comm != MPI_COMM_NULL)
-        printf("Not null %d\n", rank);
+    //if (master_comm != MPI_COMM_NULL)
+      //  printf("Not null %d\n", rank);
 
     // Scatter to grid groups as rows
     // BCast the second array to group masters
@@ -265,12 +265,12 @@ int main (int argc, char *argv[])
         */
         if (rank != 0)
             array2_colwise = malloc(N * N * sizeof(int));
-        printf("Scatterin %d\n", rank);
+        //printf("Scatterin %d\n", rank);
 
         MPI_Scatter(array1
-                , N, MPI_INT, recv_buffer
-                , N, MPI_INT, 0, master_comm);
-        printf("Scatter done on %d\n", rank);
+                , N * grid_side, MPI_INT, recv_buffer
+                , N * grid_side, MPI_INT, 0, master_comm);
+        //printf("Scatter done on %d\n", rank);
         MPI_Bcast(array2_colwise
                 , N*N, MPI_INT, 0, master_comm);
         //printf("Bcast to group masters done on %d\n", rank);
@@ -289,7 +289,7 @@ int main (int argc, char *argv[])
     int offset = (rank_row) * N * sizeof(int);
     //printf("Comm row of %d: %d\n", rank, comm_row);
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(recv_buffer + offset, N, MPI_INT, 0, comm_row);
+    MPI_Bcast(recv_buffer + offset, N * grid_side, MPI_INT, 0, comm_row);
     //printf("SCATTERING TO ROWS\n");
     MPI_Scatter(array2_colwise
             , (N * grid_side), MPI_INT, recv_col_buf
@@ -299,7 +299,7 @@ int main (int argc, char *argv[])
     //printf("Grid size is: %d\n", grid_side);
     partial_result = matrixMatrixMultSerial(recv_buffer, recv_col_buf, grid_side, N);
     MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Gather(partial_result, N * grid_side, MPI_INT
+    MPI_Gather(partial_result, grid_side * grid_side, MPI_INT
             , result_array, grid_side * grid_side, MPI_INT, 0, MPI_COMM_WORLD);
 
 
