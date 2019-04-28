4bbfecde6456b2dbf894ea4040a1fc1a4fd2efc4
egebasturk <alp.ege.basturk@gmail.com>
2019-04-28 17:21:46 +0300

    Read files are deallocated at the end now

--- Project3/cs426_project3_code/lbp_seq.c
+++ Project3/cs426_project3_code/lbp_seq.c
@@ -35,7 +35,9 @@ void create_histogram(int * hist, int ** img, int num_rows, int num_cols)
         printf("NULL Pointer\n");
     for (int i = 1; i < IMAGE_HEIGHT - 2; ++i) {
         for (int j = 1; j < IMAGE_WIDTH - 2; ++j) {
-            img_lgb[i][j] = apply_filter_on_pixel(img, i, j);
+            u_int tmp = apply_filter_on_pixel(img, i, j);
+            img_lgb[i][j] = tmp;
+            ((u_int8_t*)hist)[tmp]++;
         }
     }
     // Pixels will fit, ignore warning
@@ -54,9 +56,9 @@ void create_histogram(int * hist, int ** img, int num_rows, int num_cols)
 /// Finds the distance between two vectors
 double distance(int * a, int *b, int size)
 {
-    int sum = 0;
+    double sum = 0;
     for (int i = 0; i < size; ++i) {
-        sum += pow(a[i] - b[i], 2);
+        sum += pow(a[i] - b[i], 2) / (a[i] + b[i]) / 2;
     }
     return sqrt(sum);
 }
@@ -73,30 +75,42 @@ int main()
 {
     int k = 10, people_count = 18, sample_count_per_person = 20;
     char* buff = malloc(32 * sizeof(char));
+    int**** original_images = malloc(people_count * sizeof(int***));
+
     /// Create Arrays for each person, 18 arrays in this case
     u_int8_t*** histogram_array = malloc(people_count * sizeof(u_int8_t**));
     /// Create histograms inside portion for each person, 20 for each person
     for (int i = 0; i < people_count; ++i)
     {
         histogram_array[i] = malloc(sample_count_per_person * (sizeof(u_int8_t*)));
+        original_images[i] = malloc(sample_count_per_person * sizeof(int**));
         for (int j = 0; j < sample_count_per_person; ++j)
         {
             sprintf(buff, "../images/%d.%d.txt", i + 1, j + 1); // Arrays don't start from zero
             int** image = read_pgm_file(buff, IMAGE_HEIGHT, IMAGE_WIDTH);
+            original_images[i][j] = image;
 
-            histogram_array[i][j] = malloc(255 * (sizeof(u_int8_t)));
+            histogram_array[i][j] = calloc(256, sizeof(u_int8_t));
             create_histogram((int*)histogram_array[i][j], image, IMAGE_HEIGHT, IMAGE_WIDTH);//Dangerous cast
 
-            dealloc_2d_matrix(image, IMAGE_HEIGHT, IMAGE_WIDTH);
+            //dealloc_2d_matrix(image, IMAGE_HEIGHT, IMAGE_WIDTH);
         }
     }
+    // Test
+    sprintf(buff, "../images/%d.%d.txt", 18, 20); // Arrays don't start from zero
+
+
+    /// Cleanup
     for (int l = 0; l < people_count; ++l) {
         for (int i = 0; i < sample_count_per_person; ++i) {
             free(histogram_array[l][i]);
+            dealloc_2d_matrix(original_images[l][i], IMAGE_HEIGHT, IMAGE_WIDTH);
         }
         free(histogram_array[l]);
+        free(original_images[l]);
     }
     free(histogram_array);
+    free(original_images);
     free(buff);
     return 0;
 }
